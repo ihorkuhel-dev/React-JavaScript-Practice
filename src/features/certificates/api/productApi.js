69359ee0379d39b7dad517347e58e5b5fe3b5b71
ihorkuhel-dev <ihor.kuhel@innowise.com>
2026-04-16 11:39:17 +0200

    fix query in Api

--- src/features/certificates/api/productApi.js
+++ src/features/certificates/api/productApi.js
@@ -3,7 +3,7 @@ import { baseApi } from "../../../shared/api/baseApi";
 export const productApi = baseApi.injectEndpoints({
     endpoints: (builder) => ({
         getProducts: builder.query({
-            query: 'products',
+            query: () => 'products',
             providesTags: ['Products'],
         }),
         getProductById: builder.query({
@@ -11,7 +11,7 @@ export const productApi = baseApi.injectEndpoints({
             providesTags: (result, error, id) => [{ type: 'Products', id }]
         }),
         getFirstProduct: builder.query({
-            query: 'products/1',
+            query: () => 'products/1',
             providesTags: ['Products']
         })
     })
