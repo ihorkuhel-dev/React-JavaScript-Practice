8a87d7ca6c1afb4a8b6f6fe5c687ab0237591af3
ihorkuhel-dev <ihor.kuhel@innowise.com>
2026-04-16 16:30:33 +0200

    replace localStorage to cookie

--- src/features/auth/model/authSlice.jsx
+++ src/features/auth/model/authSlice.jsx
@@ -1,25 +1,26 @@
 import {createSlice} from "@reduxjs/toolkit";
+import Cookies from "js-cookie";
 
 const authSlice = createSlice({
     name: "auth",
     initialState: {
         user: null,
-        token: localStorage.getItem("token") || null,
-        isAuthenticated: !!localStorage.getItem("token"),
+        token: Cookies.get("token"),
+        isAuthenticated: !!Cookies.get("token"),
     },
     reducers: {
         setCredentials: (state, action) => {
-            const {token, ...user} = action.payload;
+            const {accessToken, refreshToken, ...user} = action.payload;
             state.user = user;
-            state.token = token;
-            localStorage.setItem("token", token);
+            state.token = accessToken;
             state.isAuthenticated = true;
+            Cookies.set("token", accessToken, { expires: 1 });
         },
         logout: (state) => {
             state.user = null;
             state.token = null;
             state.isAuthenticated = false;
-            localStorage.removeItem("token");
+            Cookies.remove("token");
         }
     }
 })
