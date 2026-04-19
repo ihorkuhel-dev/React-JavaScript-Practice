e659d2a4c60df6d94fa101598a01487355153241
qwerty0zero <igoricloud98@gmail.com>
2026-04-19 22:03:35 +0200

    optimize webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,80 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
-module.exports = {
-    entry: './src/app/index.jsx',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        clean: true,
-        publicPath: '/',
-    },
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                type: 'javascript/auto',
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        sourceType: 'module',
-                        presets: [
-                            ['@babel/preset-env', { targets: "defaults" }],
-                            ['@babel/preset-react', { runtime: 'automatic' }]
-                        ]
-                    }
-                }
-            },
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader',
-                ],
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-            favicon: './public/favicon.ico',
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: 'public/robots.txt', to: '.' }
+module.exports = (env, argv) => {
+    const isProd = argv.mode === 'production';
+
+    return {
+        entry: './src/app/index.jsx',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: isProd ? '[name].[contenthash:8].js' : 'bundle.js',
+            chunkFilename: isProd ? '[name].[contenthash:8].js' : '[name].js',
+            clean: true,
+            publicPath: '/',
+        },
+        resolve: {
+            extensions: ['.js', '.jsx'],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    type: 'javascript/auto',
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            cacheDirectory: true,
+                            sourceType: 'module',
+                            presets: [
+                                ['@babel/preset-env', { targets: 'defaults' }],
+                                ['@babel/preset-react', { runtime: 'automatic' }],
+                            ],
+                        },
+                    },
+                },
+                {
+                    test: /\.(sa|sc|c)ss$/,
+                    use: [
+                        isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+                        'css-loader',
+                        'sass-loader',
+                    ],
+                },
             ],
-        }),
-    ],
-    devServer: {
-        port: 3000,
-        historyApiFallback: true,
-        hot: true,
-    },
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './public/index.html',
+                favicon: './public/favicon.ico',
+            }),
+            new CopyWebpackPlugin({
+                patterns: [{ from: 'public/robots.txt', to: '.' }],
+            }),
+            ...(isProd
+                ? [
+                      new MiniCssExtractPlugin({
+                          filename: '[name].[contenthash:8].css',
+                          chunkFilename: '[id].[contenthash:8].css',
+                      }),
+                  ]
+                : []),
+        ],
+        ...(isProd
+            ? {
+                  optimization: {
+                      minimizer: ['...', new CssMinimizerPlugin()],
+                  },
+              }
+            : {}),
+        devServer: {
+            port: 3000,
+            historyApiFallback: true,
+            hot: true,
+        },
+    };
 };
