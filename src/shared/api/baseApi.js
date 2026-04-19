1fe3dfe8cce830152e4f49eef6d773d8710fa6a6
qwerty0zero <igoricloud98@gmail.com>
2026-04-19 21:30:09 +0200

    fix auth token life

--- src/shared/api/baseApi.js
+++ src/shared/api/baseApi.js
@@ -1,18 +1,29 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 import Cookies from "js-cookie";
 
+const baseQuery = fetchBaseQuery({
+    baseUrl: 'https://dummyjson.com',
+    prepareHeaders: (headers) => {
+        const token = Cookies.get('token');
+        if (token) {
+            headers.set('Authorization', `Bearer ${token}`);
+        }
+        return headers;
+    }
+});
+
+const baseQueryWithReauth = async (args, api, extraOptions) => {
+    let result = await baseQuery(args, api, extraOptions);
+    if (result.error && result.error.status === 401) {
+        Cookies.remove('token');
+        window.location.href = '/login';
+    }
+    return result;
+};
+
 export const baseApi = createApi({
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({
-        baseUrl: 'https://dummyjson.com',
-        prepareHeaders: (headers) => {
-            const token = Cookies.get('token');
-            if (token) {
-                headers.set('Authorization', `Bearer ${token}`);
-            }
-            return headers;
-        }
-    }),
+    baseQuery: baseQueryWithReauth,
     tagTypes: ['CLents', 'Auth', "Products", "User"],
     endpoints: () => ({}),
-})
+});
