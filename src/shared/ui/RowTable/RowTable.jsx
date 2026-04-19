2f434b203a6d9d610aa7245d1a375668f66b61bf
qwerty0zero <igoricloud98@gmail.com>
2026-04-19 21:29:08 +0200

    fix css

--- src/shared/ui/RowTable/RowTable.jsx
+++ src/shared/ui/RowTable/RowTable.jsx
@@ -3,15 +3,17 @@ import './RowTable.scss'
 export default function RowTable({data, className}) {
 
     return (
-        <table className={`row-table ${className}`}>
-            <tbody>
-            {data.items.map((row) => (
-                <tr key={row.id}>
-                    <th>{row.label}</th>
-                    <td>{row.value}</td>
-                </tr>
-            ))}
-            </tbody>
-        </table>
+        <div className="overflow-x-wrap">
+            <table className={`row-table ${className}`}>
+                <tbody>
+                {data.items.map((row) => (
+                    <tr key={row.id}>
+                        <th>{row.label}</th>
+                        <td>{row.value}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
     )
 }
